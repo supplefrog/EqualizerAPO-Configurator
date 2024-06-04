253e59058d7645666489123b9be6cb88dab6c46b
Akshat Das <78985073+supplefrog@users.noreply.github.com>
2024-06-04 22:24:10 +0530

    Update EQ.ps1
    Fixed

--- EQ.ps1
+++ EQ.ps1
@@ -14,47 +14,48 @@ Write-Host "5. None"
 # Get user input (accepts a number, and escape key exits the script)
 do {
     Write-Host "Enter a valid number (1-5 or Esc to exit): "
-    $key = [console]::readkey($true).keychar
+    $option = [console]::readkey($true).keychar
     # Check if Escape key was pressed
-    if ($key -eq [System.ConsoleKey]::Escape) {
+    if ($option -eq [System.ConsoleKey]::Escape) {
         Write-Host "Exiting."
         exit
     }
-    $option = switch -regex ($key) {'[0-9]' {[int]"$_"}}
-} while ($option -notin 1..5)
+} while ($option -notin "1", "2", "3", "4", "5")
+
+$content = Get-Content $file
 
 # Modify specific lines based on user option
 switch ($option) {
-    1 {
-        # Modify Preamp and Include lines for Buds FE Binaural
+   '1' {
+        # Buds FE Binaural
         $content[-4] = "Preamp: -25.8 dB"
         $content[-3] = "Include: HeSuVi\hesuvi.txt"
         $content[-2] = "# Include: AIW_WZ3_S2v1.txt"
         $content[-1] = "Include: DynamiQ-master\DynamiQ.txt"
     }
-    2 {
-        # Modify Preamp and Include lines for Buds FE Stereo
+   '2' {
+        # Buds FE Stereo
         $content[-4] = "Preamp: -10.7 dB"
         $content[-3] = "Include: HeSuVi\hesuvi.txt"
         $content[-2] = "# Include: AIW_WZ3_S2v1.txt"
         $content[-1] = "Include: DynamiQ-master\DynamiQ.txt"
     }
-    3 {
-        # Modify Preamp and Include lines for Buds FE Warzone
+   '3' {
+        # Buds FE Warzone
         $content[-4] = "Preamp: -19.5 dB"
         $content[-3] = "Include: HeSuVi\hesuvi.txt"
         $content[-2] = "Include: AIW_WZ3_S2v1.txt"
         $content[-1] = "Include: DynamiQ-master\DynamiQ.txt"
     }
-    4 {
-        # Modify Preamp and Include lines for Warzone
+   '4' {
+        # Warzone
         $content[-4] = "Preamp: -21.9 dB"
         $content[-3] = "Include: HeSuVi\hesuvi.txt"
         $content[-2] = "Include: AIW_WZ3_S2v1.txt"
         $content[-1] = "Include: DynamiQ-master\DynamiQ.txt"
     }
-    5 {
-        # Comment out Preamp and Include lines
+   '5' {
+        # None - Comment out lines
         $content[-4] = "# Preamp: -21.9 dB"
         $content[-3] = "# Include HeSuVi\hesuvi.txt"
         $content[-2] = "# Include: AIW_WZ3_S2v1.txt"
@@ -68,13 +69,14 @@ switch ($option) {
 
 # Open the corresponding shortcut
 switch ($option) {
-    1 { Start-Process $shortcut1 }
-    2 { Start-Process $shortcut2 }
-    3 { Start-Process $shortcut3 }
-    4 { Start-Process $shortcut4 }
+    '1' { Start-Process $shortcut1 }
+    '2' { Start-Process $shortcut2 }
+    '3' { Start-Process $shortcut3 }
+    '4' { Start-Process $shortcut4 }
 }
 
 # Write modified content back to $file
 $content | Set-Content -Path $file
 
+# Exit the script
 exit
