a793d0cccd8cfbe527c460ba1e9fac875595f219
supplefrog <78985073+supplefrog@users.noreply.github.com>
2025-06-28 04:44:15 +0000

    Create download.ps1

---
+++ download.ps1
@@ -0,0 +1,9 @@
+$DIR = "C:\Users\E\AppData\Roaming\Microsoft\Windows\Start Menu"
+if (-Not (Test-Path $DIR)) { Write-Error "$DIR not found"; exit }
+@(
+  "EQ.ps1",
+  "EQ.ps1.lnk"
+) | ForEach-Object {
+  Invoke-WebRequest "https://raw.githubusercontent.com/supplefrog/EqualizerAPO-Configurator/refs/heads/main/$_" -OutFile (Join-Path $DIR $_)
+  "Downloaded $_ to $DIR"
+}
